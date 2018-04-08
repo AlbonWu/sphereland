e4cba03087a14326d81a57a85faa7e59c3e627bd
Albon Wu <albonyaowu@gmail.com>
2018-04-08 14:22:50 -0500

    Add files via upload

---
+++ Actions.java
@@ -0,0 +1,132 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.*;
+import java.awt.geom.*;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Actions extends JPanel implements ActionListener, KeyListener {
+
+	Timer t = new Timer(5, this);
+	// used to move the shape
+	Rectangle2D r;
+
+	double x = 0, y = 0, velx = 0, vely = 0;
+	// x, y are the coordinates and velx, vely describe the increments
+	int width;
+	int height;
+	
+	public Actions(int width, int height) {
+		t.start();
+		// start the timer
+
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+
+		width = this.width;
+		height = this.height;
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		r = new Rectangle2D.Double(x, y, 40, 40);
+		g2.fill(r);
+		// draw the rectangle
+
+		Shapes s = new Shapes();
+		s.paintComponent(g2);
+		// access class Shapes which draws the houses
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (x < 0 || x > 800) {
+			velx *= -1;
+		}
+		if (y < 0 || y > 800) {
+			vely *= -1;
+		}
+		
+		Shapes s = new Shapes();
+		
+		
+		x += velx;
+		y += vely;
+		// change the coordinates
+
+		repaint();
+		// redraw the square in the incremented positions
+	}
+
+	public void up() {
+		vely = -2;
+		velx = 0;
+	}
+
+	public void down() {
+		vely = 2;
+		velx = 0;
+	}
+
+	public void left() {
+		velx = -2;
+		vely = 0;
+	}
+
+	public void right() {
+		velx = 2;
+		vely = 0;
+	}
+
+	public void stop() {
+		velx = 0;
+		vely = 0;
+	}
+	// how much in each direction the coordinates should be moved
+
+	public void keyPressed(KeyEvent e) {
+		int code = e.getKeyCode();
+		if (code == KeyEvent.VK_W) {
+			up();
+		}
+		if (code == KeyEvent.VK_S) {
+			down();
+		}
+		if (code == KeyEvent.VK_A) {
+			left();
+		}
+		if (code == KeyEvent.VK_D) {
+			right();
+		}
+	}
+	// detect the keys pressed and increment accordingly
+
+	public void keyTyped(KeyEvent e) {
+	}
+
+	public void keyReleased(KeyEvent e) {
+		int code = e.getKeyCode();
+		if (code == KeyEvent.VK_W) {
+			stop();
+		}
+		if (code == KeyEvent.VK_S) {
+			stop();
+		}
+		if (code == KeyEvent.VK_A) {
+			stop();
+		}
+		if (code == KeyEvent.VK_D) {
+			stop();
+		}
+	}
+	// stop movement when keys are released
+
+	public static boolean isColliding(Rectangle2D rect1, Line2D line2) {
+		if (line2 != null) {
+			return line2.intersects(rect1);
+		}
+		return false;
+	}
+}
---
+++ Shapes.java
@@ -0,0 +1,54 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.geom.Line2D;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Shapes extends JPanel {
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		house1(g);
+		house2(g);
+		house3(g);
+		square1(g);
+	}
+
+	public void house1(Graphics g) {
+		Polygon p = new Polygon();
+		for (int i = 0; i < 5; i++) {
+			p.addPoint((int) (200 + 90 * Math.cos(i * 2 * Math.PI / 5)),
+					(int) (100 + 90 * Math.sin(i * 2 * Math.PI / 5)));
+		}
+		Graphics2D g2 = (Graphics2D) g;
+		Line2D line2 = new Line2D.Double(127, 46, 127, 130);
+		
+		g2.draw(line2);
+		g.drawPolygon(p);
+	}
+	
+	public void house2(Graphics g) {
+		Polygon p = new Polygon();
+		for (int i = 0; i < 5; i++) {
+			p.addPoint((int) (400 + 90 * Math.cos(i * 2 * Math.PI / 5)),
+					(int) (200 + 90 * Math.sin(i * 2 * Math.PI / 5)));
+		}
+		g.drawPolygon(p);
+	}
+	
+	public void house3(Graphics g) {
+		Polygon p = new Polygon();
+		for (int i = 0; i < 5; i++) {
+			p.addPoint((int) (600 + 90 * Math.cos(i * 2 * Math.PI / 5)),
+					(int) (100 + 90 * Math.sin(i * 2 * Math.PI / 5)));
+		}
+		g.drawPolygon(p);
+	}
+	
+	public void square1(Graphics g){
+		g.fillRect(195, 30, 40, 40);
+	}
+	
+}
